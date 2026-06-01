eee8f6cc5800aed8e600027db052cf265c6d7be0
IgorPacheco1 <igopacheco7@gmail.com>
2026-06-01 14:59:47 -0300

    Implementação de consultas: FIX: trocando o intersect pelo inner join, pois o mysql não tem o intersect

--- sql/Consultas.sql
+++ sql/Consultas.sql
@@ -5,6 +5,7 @@ join plano p on m.id_plano = p.id_plano;
 
 -- no caso esses c, m, e p são para diferenciar do nome da tabela, se nao ia ficar muito confuso, caso alguem tenha uma sugestão de um nome melhor já pode alterar 
 
-select c.nome from cliente c join matricula m on c.id_cliente = m.id_cliente
-intersect
-select c.nome from cliente c join t on c.id_cliente = t.id_cliente;
+select distinct c.nome
+from cliente c
+inner join matricula m on c.id_cliente = m.id_cliente
+inner join treino t on c.id_cliente = t.id_cliente;
