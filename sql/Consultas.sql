bd6d28212e7120155aa0e7943d569df76f9b76c3
IgorPacheco1 <igopacheco7@gmail.com>
2026-06-01 14:56:00 -0300

    Implementação de consultas: Intersect

--- sql/Consultas.sql
+++ sql/Consultas.sql
@@ -4,3 +4,7 @@ join matricula m on c.id_cliente = m.id_cliente
 join plano p on m.id_plano = p.id_plano;
 
 -- no caso esses c, m, e p são para diferenciar do nome da tabela, se nao ia ficar muito confuso, caso alguem tenha uma sugestão de um nome melhor já pode alterar 
+
+select c.nome from cliente c join matricula m on c.id_cliente = m.id_cliente
+intersect
+select c.nome from cliente c join t on c.id_cliente = t.id_cliente;
