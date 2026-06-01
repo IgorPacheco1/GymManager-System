32e5f8cb70d20d8c8ae17578f026d6b25e953f8f
IgorPacheco1 <igopacheco7@gmail.com>
2026-06-01 14:00:33 -0300

    Criação da tabela matricula

--- sql/Tabela.sql
+++ sql/Tabela.sql
@@ -20,3 +20,14 @@ create table plano(
 id_plano int primary key auto_increment,
 nome_plano varchar(50) not null,
 valor decimal(10,2) not null);
+
+create table matricula(
+id_matricula int primary key auto_increment,
+id_cliente int not null,
+id_plano int not null,
+data_inicio date not null,
+foreign key (id_cliente) references cliente(id_cliente),
+foreign key (id_plano) references plano(id_plano)
+);
+
+
