1175b8184d99a3d46c57d56651e6acabaf88686f
IgorPacheco1 <igopacheco7@gmail.com>
2026-06-02 17:30:24 -0300

    Testando conexão do C# com o banco de dados

--- src/GymManager/Program.cs
+++ src/GymManager/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using System;
+using MySql.Data.MySqlCliente;
+using GymManager.Data;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ConexaoDB conexao = new ConexaoDB();
+
+        try
+        {
+            using (MySqlConexao conect = conexao.ObterConexao())
+            {
+                conect.Open();
+                System.Console.WriteLine("Deu bom");
+            }
+        }
+        catch (System.Exception)
+        {
+
+            System.Console.WriteLine("Deu ruim" + ex.Message);
+        }
+    }
+}
