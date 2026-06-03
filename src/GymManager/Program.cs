7a0433ab703bb314aa5b1a7f0b8d726e6d816af4
IgorPacheco1 <igopacheco7@gmail.com>
2026-06-03 14:33:25 -0300

    Criando teste para verificação de inserção no banco de dados

--- src/GymManager/Program.cs
+++ src/GymManager/Program.cs
@@ -1,25 +1,47 @@
 ﻿using System;
 using MySql.Data.MySqlClient;
 using GymManager.Data;
+using GymManager.Models;
+using GymManager.Repositories;
 
 class Program
 {
     static void Main(string[] args)
     {
-        ConexaoDB conexao = new ConexaoDB();
+        // ConexaoDB conexao = new ConexaoDB();
+
+        // try
+        // {
+        //     using (MySqlConnection conect = conexao.ObterConexao())
+        //     {
+        //         conect.Open();
+        //         System.Console.WriteLine("Deu bom");
+        //     }
+        // }
+        // catch (System.Exception)
+        // {
+
+        //     System.Console.WriteLine("Deu ruim");
+        // }
+
+        Cliente novoCliente = new Cliente
+        {
+            IdUsuario = 1,
+            Nome = "teste",
+            Cpf = "00000000011",
+            Telefone = "40028922"
+        };
 
         try
         {
-            using (MySqlConnection conect = conexao.ObterConexao())
-            {
-                conect.Open();
-                System.Console.WriteLine("Deu bom");
-            }
+            ClienteRepository repo = new ClienteRepository();
+            repo.Adicionar(novoCliente);
+            System.Console.WriteLine("Cliente inserido no bando de dados");
         }
-        catch (System.Exception)
+        catch
         {
-
-            System.Console.WriteLine("Deu ruim");
+            System.Console.WriteLine("Cliente não inserido no banco de dados");
         }
+
     }
 }
