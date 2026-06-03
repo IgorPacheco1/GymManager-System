f2baf7c08ba9868dcf0374979827ac4839e62068
IgorPacheco1 <igopacheco7@gmail.com>
2026-06-03 14:13:52 -0300

    Criação da classe Cliente

---
+++ src/GymManager/Models/Cliente.cs
@@ -0,0 +1,12 @@
+namespace GymManager.Models
+{
+    public class Cliente
+    {
+        public int IdCliente{get; set;}
+        public int IdUsuario{get; set;}
+        public string Nome{get; set;}
+        public string Cpf {get; set;}
+        public string Telefone{ get; set;}
+
+    }
+}
